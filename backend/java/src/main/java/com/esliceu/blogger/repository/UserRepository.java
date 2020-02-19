03ebe368ab115067c2cf09c100fe75d3eec52779
marcos <marcosvalens@gmail.com>
2020-02-19 20:56:14 +0100

    Marcos - UserRepository

---
+++ backend/java/src/main/java/com/esliceu/blogger/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.esliceu.blogger.repository;
+
+import com.esliceu.blogger.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByEmailAndPassword(String mail, String password);
+
+    User findByUsername(String userName);
+}
