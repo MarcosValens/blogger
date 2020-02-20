8a5f6b9dc8b8a242f6ec5087ac853fd31afc9f4a
tonirigo <tonirigo16@gmail.com>
2020-02-20 20:34:31 +0100

    LoginLayout and Login page created

---
+++ frontend/src/layouts/LoginLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+      <h1>Login</h1>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+<style scoped>
+    h1{
+        text-align: center;
+    }
+</style>
+<script>
+export default {
+    name: 'LoginLayout',
+  data () {
+    return {
+    };
+  },
+};
+</script>
---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-input v-model="email" label="E-mail"/>
+    <q-input v-model="password" type="password" label="password"/>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data(){
+    return {
+        email:'',
+        password:''
+    }
+  }
+};
+</script>
