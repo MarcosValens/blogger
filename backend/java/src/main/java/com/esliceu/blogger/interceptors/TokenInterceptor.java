6bceee5b00548f5f49473bcd8097db462341719b
rochismo <alexis.platino@gmail.com>
2020-02-20 17:59:34 +0100

    Benjamín: Added interceptors package and added TokenInterceptor class, implementation is left to do.

---
+++ backend/java/src/main/java/com/esliceu/blogger/interceptors/TokenInterceptor.java
@@ -0,0 +1,13 @@
+package com.esliceu.blogger.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TokenInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+}
