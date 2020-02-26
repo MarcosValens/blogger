61895aa7c93061b77af5bbd20adda694a85ddfec
rochismo <alexis.platino@gmail.com>
2020-02-26 16:43:37 +0100

    Benjamin: Accepted toni's changes

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -11,14 +11,9 @@ const routes = [
     path: '/blogger/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-<<<<<<< HEAD
-      { path: 'all', component: () => import('pages/Index.vue') },
-      { path: 'create', component: () => import('pages/BlogForm.vue') }
-=======
       { path: '', component: () => import('pages/Index.vue')},
       { path: 'create', component: () => import('pages/BlogForm.vue')},
       { path: 'calorieCalculator', component: () => import('pages/CalorieCalculator.vue')}
->>>>>>> eba1a82b995f45f3bff05bbba1b014c6e6d6f176
     ]
   },
 ];
