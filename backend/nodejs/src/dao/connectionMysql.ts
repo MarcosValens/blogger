a4182cd83a426860ae8819baa235ca43082060f7
Marcos <marcosvalens@gmail.com>
2020-03-03 16:05:30 +0100

    commit para pull

--- backend/nodejs/src/dao/connectionMysql.ts
+++ backend/nodejs/src/dao/connectionMysql.ts
@@ -2,16 +2,13 @@ let mysql = require('mysql');
 const mysqlHost:string = process.env.MYSQL_HOST;
 const mysqlUser:string = process.env.MYSQL_USER;
 const mysqlPassword:string = process.env.MYSQL_PASSWORD;
-var con = mysql.createConnection({
+let connection = mysql.createConnection({
     host: mysqlHost,
     user: mysqlUser,
     password: mysqlPassword
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+connection.connect();
 
 /*
 return new Promise((resolve, reject) => {
